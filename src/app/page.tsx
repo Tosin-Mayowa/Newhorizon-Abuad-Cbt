a39fedb50da38acede6432f4b53666269453c2a7
Tosin-Mayowa <toss800@gmail.com>
2025-10-15 16:48:02 +0100

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,43 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <section className="w-screen h-full">
+      <header className="bg-primary text-white border border-solid border-black w-full lg:h-[80px] flex justify-around items-center">
+        <h1 className="lg:text-[30px] font-bold">CBT-App</h1>
+        <nav className="w-[60%] h-full border border-solid border-red-500 flex justify-end">
+          <ul className="w-[60%] h-full flex justify-between items-center">
+            <li className="">
+              <Link href="/" className="font-extrabold text-[18px]">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <Link href="/login">Login</Link>
+          </ul>
+        </nav>
+      </header>
+      {/* Home page content */}
+      <div className="flex justify-between lg:h-full w-full bg-foreground">
+        <div className="lg:w-[60%] lg:h-full flex flex-col justify-center border border-solid border-black pl-4">
+          <h1 className="lg:text-[40px]  text-secondary font-bold">🎓 Welcome to <span className="text-primary">New Horizon</span>  CBT Portal</h1>
+          <div className="w-[80%] lg:h-[25%]  self-center flex flex-col justify-between border border-solid border-black">
+           <div>
+             <h3 className="lg:text-[20px] text-primary font-bold ">Powered in Partnership with Afe Babalola University (ABUAD)</h3>
+          <p className="lg:text-[16px] text-primary font-light ">
+            Prepare, Practice, and Perform with Confidence. Our Computer-Based
+            Test (CBT) platform is designed to deliver a smooth, secure, and
+            efficient examination experience for every student.
+          </p>
+           </div>
+          <button className="lg:w-[50%] bg-primary lg:h-[40px] rounded-[8px] text-white font-bold cursor-pointer">Learn More</button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div></div>
+      </div>
+    </section>
   );
 }
